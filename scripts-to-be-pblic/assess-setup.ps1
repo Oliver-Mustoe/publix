4490d8f4ecda55ddef22655a0bc5b1c03342227d
Olive <71083461+Oliver-Mustoe@users.noreply.github.com>
2023-03-01 16:13:38 -0500

    Update assess-setup.ps1

--- scripts-to-be-pblic/assess-setup.ps1
+++ scripts-to-be-pblic/assess-setup.ps1
@@ -20,7 +20,7 @@ $cred = New-Object System.Management.Automation.PSCredential $username, $Passwor
 Rename-Computer -NewName $hostname
 
 # Add ssh-keys
-Start-Process Powershell -LoadUserProfile -Credential $Cred -NoNewWindow -wait -ArgumentList'-Command',"ssh-keygen -t rsa -b 4096 -f C:\Users\$Username\.ssh\$hostname-jump-keys -C $hostname"
+Start-Process Powershell -LoadUserProfile -Credential $Cred -wait -ArgumentList'-Command',"ssh-keygen -t rsa -b 4096 -f C:\Users\$Username\.ssh\$hostname-jump-keys -C $hostname"
 
 # log off
 Write-Host "Logging off"
