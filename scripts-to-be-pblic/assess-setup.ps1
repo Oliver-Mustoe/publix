52d06d62940ca2afd7afe21d6117b5813fe45ea5
Olive <71083461+Oliver-Mustoe@users.noreply.github.com>
2023-03-02 10:27:48 -0500

    Update assess-setup.ps1

--- scripts-to-be-pblic/assess-setup.ps1
+++ scripts-to-be-pblic/assess-setup.ps1
@@ -13,7 +13,7 @@ $Username = Read-Host -prompt "What is the named administrative user you want to
 $Password = Read-Host -prompt "What is the password for this user?" -AsSecureString
 $hostname = Read-Host -prompt "What is the hostname of this box supposed to be?"
 New-LocalUser $Username -Password $Password -AccountNeverExpires -PasswordNeverExpires:$true
-Add-LocalGroupMember -Group "Administrators" -Member $cred
+Add-LocalGroupMember -Group "Administrators" -Member $Username
 $cred = New-Object System.Management.Automation.PSCredential $Username, $Password
 
 # Rename computer
@@ -21,6 +21,7 @@ Rename-Computer -NewName $hostname
 
 # Start-Process Powershell "-Command Read-host((whoami))" -LoadUserProfile -NoNewWindow -Credential $Cred -wait
 # Start-Process Powershell "-Command ssh-keygen -t rsa -b 4096 -f C:\Users\$Username\$Username-jump-keys -C $Username" -LoadUserProfile -Credential $Cred -wait -NoNewWindow
-Start-Process Powershell "-Command ssh-keygen -t rsa -b 4096 -f C:\Users\$Username\.ssh\$Username-jump-keys -C $Username" -Credential $cred -wait
+Start-Process Powershell "mkdir C:\Users\$Username\.ssh" -Credential $cred -wait
+Start-Process Powershell "ssh-keygen -t rsa -b 4096 -f C:\Users\$Username\.ssh\$Username-jump-keys -C $Username" -Credential $cred -wait
 # log off
 Write-Host "When ready, reboot!"
